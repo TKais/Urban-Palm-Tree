ca1c6593e162f51637cd02e11cac561a85f8c441
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-07-25 11:27:27 -0700

    Add global styles to :root

--- frontend/components/styles/Main.js
+++ frontend/components/styles/Main.js
@@ -1,7 +1,15 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --green: #65B8BF;
+    --hotpink: #F96CFF;
+    --maxWidth: 1200px;
+  }
+`;
 
 export const LogoStyles = styled.h1`
-  background: red;
+  background: var(--hotpink);
   font-size: 4rem;
   transform: skew(-7deg);
   margin-left: 2rem;
@@ -9,5 +17,21 @@ export const LogoStyles = styled.h1`
     color: white;
     text-decoration: none;
     padding: 0.5rem 1rem;
+  }
+`;
+
+export const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .search {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
   }
 `;
