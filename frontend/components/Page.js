9cc49541ce85616b499dc4ca11c9a0d4d09d8957
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-07-25 11:28:24 -0700

    Add global styles to Page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 import Header from './Header';
+import { GlobalStyles } from './styles/Main';
 
 function Page(props) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
       <p>I'm the page component</p>
       {props.children}
