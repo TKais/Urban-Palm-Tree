800206b716d321e4b55f106c4b4ba4f672946efd
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-07-25 09:18:33 -0700

    Add styled components folder

---
+++ frontend/components/styles/Main.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LogoStyles = styled.h1`
+  background: red;
+  font-size: 4rem;
+  transform: skew(-7deg);
+  margin-left: 2rem;
+  a {
+    color: white;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+  }
+`;
