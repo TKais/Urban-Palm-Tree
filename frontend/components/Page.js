d3d13f6fdee3e67cbf8f0c3b2af7fe94e5dffea0
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-07-25 12:50:18 -0700

    Add InnerStyles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
 import Header from './Header';
-import { GlobalStyles } from './styles/Main';
+import { GlobalStyles, InnerStyles } from './styles/Main';
 
 function Page(props) {
   return (
     <div>
       <GlobalStyles />
       <Header />
-      <p>I'm the page component</p>
-      {props.children}
+      <InnerStyles>
+        {props.children}
+      </InnerStyles>
     </div>
   )
 }
