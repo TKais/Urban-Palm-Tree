b84aeb21c8a55fbdeaa8b5a28f9142f6755c29e8
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-07-25 09:18:04 -0700

    Add styled logo component

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,12 +1,19 @@
+import Link from 'next/link';
+import Nav from './Nav';
+import { LogoStyles } from './styles/Main';
+
 function Header(props) {
   return (
     <header>
       <div className="bar">
-        Urban Palm Tree
+        <LogoStyles>
+          <Link href="/">Urban Palm Tree</Link>
+        </LogoStyles>
       </div>
       <div className="search">
         <p>Search placeholder</p>
       </div>
+      <Nav />
     </header>
   );
 }
